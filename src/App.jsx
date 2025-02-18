cce62b9beb14d0e52f49bd2c9c0bea2fe439dc77
Zackp5600 <zackp5600@gmail.com>
2025-02-17 20:56:57 -0500

    did sumn idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
@@ -8,11 +9,18 @@ function App() {
 
   return (
     <>
-      <h1>Test Website</h1>
+    <div className='top' >
+      <div>
+        <img src="src/assets/openai.png" className='image' />
+      </div>
+      <div>
+        <h1>Test Website</h1>
+      </div>
+    </div>
       <div className="card-container">
         <div className="card"> 
           <h2>Example Button: </h2>
-          <button onClick={() => setCount((count) => count + 1)}>
+          <button onClick={() => setCount((count) => count + 1)} className='btn btn-danger'>
             Count: {count}
           </button>
         </div>
@@ -20,13 +28,14 @@ function App() {
         {count > 5 ?         
         <div className="card"> 
           <h2>Secret Button: </h2>
-          <button onClick={() => setCount((count) => count * 2)}>
+          <button onClick={() => setCount((count) => count * 2)} className='btn btn-warning '>
             Count: {count}
           </button>
         </div>
         : null }
 
       </div>
+    
     </>
   )
 }
